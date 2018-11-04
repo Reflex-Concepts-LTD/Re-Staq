ebda41792ba185969927e9791b5dc7ef8c66c8a1
Think Binary. Think Code <hello@reflexconcepts.co.ke>
2018-11-04 10:36:05 +0300

    Partners Added.
    Site ready for upload in my view.

--- core/database/include.php
+++ core/database/include.php
@@ -1,7 +1,7 @@
 <?php
 //Change host name, database type, database, username, password appropriately in the configs file
 
-$configs = parse_ini_file(WPATH . "core/configs.ini");
+$configs = parse_ini_file(WPATH . "core/configs_lb.ini");
 require "Database.php";
 Database::setUp(array(
     'dsn' => 'mysql:host=' . $configs["host_name"] . ';dbname=' . $configs["db_name"] . ';',
--- modules/partners.php
+++ modules/partners.php
@@ -19,46 +19,119 @@
     </div>
     <!-- ***** Page Top End ***** -->
 
-    <!-- ***** Page Content Start ***** -->
-    <div class="page-bottom pbottom-70">
+    <!-- ***** Our Team Start ***** -->
+    <section class="section pbottom-70">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Proin pellentesque quam at quam aliquet, eget mattis quam euismod. Curabitur imperdiet suscipit facilisis. Maecenas diam quam, sodales eu est eu, posuere ultricies augue. Sed rhoncus facilisis risus, nec sagittis dui luctus in. Nunc eget ante odio. Suspendisse eu diam ut nisi ullamcorper interdum. Pellentesque sit amet quam risus.</p>
-                    <!-- ***** Gallery Start ***** -->
-<!--                    <div class="row page-gallery-wrapper">
-                        <div class="col-lg-4 col-md-4 col-sm-12">
-                            <a href="images/photos/about/1.jpg" class="page-gallery" title="About Us">
-                                <img src="images/photos/about/1.jpg" alt="">
-                            </a>
-                        </div>
-                        <div class="col-lg-4 col-md-8 col-sm-12">
-                            <div class="row">
-                                <div class="col-lg-12 col-md-6">
-                                    <a href="images/photos/about/2.jpg" class="page-gallery small" title="About Us">
-                                        <img src="images/photos/about/2.jpg" alt="">
-                                    </a>
-                                </div>
-                                <div class="col-lg-12 col-md-6">
-                                    <a href="images/photos/about/3.jpg" class="page-gallery small" title="About Us">
-                                        <img src="images/photos/about/3.jpg" alt="">
-                                    </a>
-                                </div>
+                    <div class="center-heading">
+                        <h2 class="section-title">Our Partners</h2>
+                    </div>
+                </div>
+                <div class="offset-lg-3 col-lg-6">
+                    <div class="center-text">
+                        <p>We have partners with industry experts.</p>
+                    </div>					
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                    <div class="team-item">
+                        <div class="header">
+                            <div>
+                                <img src="images/partners/ict_innovators.svg" width="180" alt="">
+                            </div>
+                            <br/>
+                            <div class="info">
+                                <strong>ICT Innovators Cooperatives</strong>
+                                <span>A PAN African SACCO.</span>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-4 col-sm-12">
-                            <a href="images/photos/about/4.jpg" class="page-gallery" title="About Us">
-                                <img src="images/photos/about/4.jpg" alt="">
-                            </a>
+<!--                        <div class="body">
+                            Proin arcu ligula, malesuada id tincidunt laoreet, facilisis at justo. Sed at lorem malesuada.
+                        </div>-->
+                        <ul class="social">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-github"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                    <div class="team-item">
+                        <div class="header">
+                            <div>
+                               <img src="images/partners/rc.svg" width="180" alt="">
+                            </div>
+                             <br/>
+                            <div class="info">
+                                <strong>Reflex Concepts [KE, UG] LTD</strong>
+                                <span>A start-up for start-ups.</span>
+                            </div>
                         </div>
-                    </div>-->
-                    <!-- ***** Gallery End ***** -->
-                    <p>Quisque quis faucibus tellus, non tincidunt urna. Pellentesque faucibus aliquet mi, id efficitur nisl aliquam at. Phasellus volutpat lobortis consectetur. Duis efficitur iaculis ipsum ut sodales. Phasellus suscipit lectus eget sapien accumsan, vitae auctor magna suscipit. Ut sagittis mauris auctor dictum feugiat. Duis lectus neque, aliquet quis justo euismod, rutrum vehicula felis. In luctus dui nec pretium gravida. In mauris lorem, accumsan sit amet lacinia non, auctor id dui. </p>
+<!--                        <div class="body">
+                            Proin arcu ligula, malesuada id tincidunt laoreet, facilisis at justo. Sed at lorem malesuada.
+                        </div>-->
+                        <ul class="social">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-github"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                    <div class="team-item">
+                        <div class="header">
+                            <div>
+                               <img src="images/partners/moba.svg" width="180" alt="">
+                            </div>
+                             <br/>
+                            <div class="info">
+                                <strong>MOBA SACCO</strong>
+                                <span>A SACCO for Maseno Old Boys</span>
+                            </div>
+                        </div>
+<!--                        <div class="body">
+                            Proin arcu ligula, malesuada id tincidunt laoreet, facilisis at justo. Sed at lorem malesuada.
+                        </div>-->
+                        <ul class="social">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-github"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                    <div class="team-item">
+                        <div class="header">
+                            <div>
+                               <img src="images/partners/ec.svg" width="180" alt="">
+                            </div>
+                             <br/>
+                            <div class="info">
+                                <strong>Eunique Capital Investments</strong>
+                                <span>A SACCO for small-scale traders in Mwiki.</span>
+                            </div>
+                        </div>
+<!--                        <div class="body">
+                            Proin arcu ligula, malesuada id tincidunt laoreet, facilisis at justo. Sed at lorem malesuada.
+                        </div>-->
+                        <ul class="social">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-github"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- ***** Page Content End ***** -->
+    </section>
+    <!-- ***** Our Team End ***** -->
+
 </section>
 
 <!-- ***** Parallax Start ***** -->
