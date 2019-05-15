b06c3ea6fb3eb0edbb36787b2dd035765820d5df
Think Binary. Think Code <hello@reflexconcepts.co.ke>
2019-05-15 22:11:49 +0300

    Merger.

--- modules/inc/home/about_sec.php
+++ modules/inc/home/about_sec.php
@@ -19,24 +19,24 @@
             <div class="offset-lg-1 col-lg-6 col-md-12 col-sm-12 align-self-bottom">
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                        <a href="?cross_platform" class="home-services-item mtop-70" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.2s">
+                        <a href="?#" class="home-services-item mtop-70" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.2s">
                             <i class="fa fa-clone"></i>
                             <h5 class="services-title">Cross Platform</h5>
                             <p>Staqpesa is accessible from all web-based computing and mobile devices. </p>
                         </a>
-                        <a href="?saas" class="home-services-item" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.2s">
+                        <a href="?#" class="home-services-item" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.2s">
                             <i class="fa fa-connectdevelop"></i>
                             <h5 class="services-title">SaaS</h5>
                             <p>No need for infrastructural setup. Just subscribe to any of our affordable packages to use Staqpesa as a service. </p>
                         </a>
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                        <a href="?scalability" class="home-services-item active" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.3s">
+                        <a href="?#" class="home-services-item active" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.3s">
                             <i class="fa fa-object-ungroup"></i>
                             <h5 class="services-title">Scalable</h5>
                             <p>Staqpesa is easily customizable to suit your needs, enforce your unique by-laws, policies and regulations.</p>
                         </a>
-                        <a href="?reporting_analytics" class="home-services-item" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.3s">
+                        <a href="?#" class="home-services-item" data-scroll-reveal="enter bottom move 30px over 0.6s after 0.3s">
                             <i class="fa fa-line-chart"></i>
                             <h5 class="services-title">Reporting and Analytics</h5>
                             <p>Staqpesa generates and avails all the necessary reports and statements.</p>
